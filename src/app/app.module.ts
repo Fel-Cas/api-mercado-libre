3e93a63661ae3afb1e449cc0e07cdbfab483a0c2
Andrés Castro <andres.cmonsalve@gmail.com>
2022-06-04 10:42:41 -0500

    Feature: Buscar un producto por el nombre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent,
+    ItemDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/items-list/items-list.component.ts
+++ src/app/components/items-list/items-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-items-list',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsListComponent implements OnInit {
 
-  constructor() { }
+  // Formulario 
+  formItem: FormGroup;
+  // Items que me traiga la API
+  items:any[];
+  
+  
+  constructor(
+    private itemService: ItemsService
+  ) {
+    this.items=[];
+    this.formItem= new FormGroup({
+      itemName: new FormControl('',[
+        Validators.required,
+        Validators.minLength(3)
+      ])
+    }) 
+
+  }
 
   ngOnInit(): void {
   }
 
+  async onSubmit(){
+    let itemNameToSearch=this.formItem.value?.itemName;
+    let itemsFound:any=await this.itemService.searchItem(itemNameToSearch);
+    this.items=itemsFound['results'];
+    this.formItem.reset()
+  }
 }
