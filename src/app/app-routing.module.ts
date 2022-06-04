bf1b9441438b0d08756e3537bc2b114ad6eca48a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-06-04 09:49:15 -0500

    Feature: Creación de los componetes y las rutas que los van a mostrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { ItemsListComponent } from './components/items-list/items-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'items', component:ItemsListComponent},
+  {path:'item-detail/:item', component:ItemDetailComponent},
+  {path:'', pathMatch:'full', redirectTo:'items'},
+  {path:'**', redirectTo:'items'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
